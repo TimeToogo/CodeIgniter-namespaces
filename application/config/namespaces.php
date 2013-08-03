3540e4d9d69e0f23a60924a3bb9027e12e0fd265
Time Toogo <lello-3@hotmail.com>
2013-08-03 12:38:10 +1000

    Initial commit

---
+++ application/config/namespaces.php
@@ -0,0 +1,12 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+define('BASE_NAMESPACE', '\DebtCollect\UI\application\\');
+
+define('CONTROLLER_NAMESPACE', '\controllers\\');
+define('MODEL_NAMESPACE', '\models\\');
+define('VIEW_NAMESPACE', '\views\\');
+define('HELPER_NAMESPACE', '\helpers\\');
+define('LIBRARY_NAMESPACE', '\libraries\\');
+
+/* End of file namespaces.php */
+/* Location: ./application/config/namespaces.php */
